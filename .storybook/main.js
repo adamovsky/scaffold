64b8827e90a250d9f9c3c5ef100f4b37cb3f0238
Milan Adamovsky <madamovsky@humana.com>
2021-10-03 17:19:33 -0400

    Add storybook configs

---
+++ .storybook/main.js
@@ -0,0 +1,11 @@
+module.exports = {
+  "stories": [
+    "../src/components/*/stories.js",
+    "../src/pages/*/stories.js"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/preset-create-react-app"
+  ]
+}
---
+++ .storybook/preview.js
@@ -0,0 +1,19 @@
+import { MemoryRouter } from 'react-router-dom';
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+}
+
+export const decorators = [
+  (Story) => (
+    <MemoryRouter>
+      <Story />
+    </MemoryRouter>
+  ),
+];
